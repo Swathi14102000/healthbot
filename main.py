8ec4748d62dcbc1f4743d772d4e21a5561940f96
Swathi M <swathim14102000@gmail.com>
2025-06-08 18:02:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ mycursor.execute("CREATE DATABASE chatbot ")
 mycursor.execute("""CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE,
-    email VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
     password_hash VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)""")
 
