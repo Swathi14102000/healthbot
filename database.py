783f439be33856b3e848b7282503d1c29025e7ad
Swathi14102000 <swathim14102000@gmail.com>
2025-06-25 18:00:14 +0530

    updated code

--- database.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-URL_DATABASE = 'mysql+pymysql://root:root@localhost:3306/chatbot'
-engine = create_engine(URL_DATABASE)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False,bind=engine)
-Base = declarative_base()
